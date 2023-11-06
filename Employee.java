293bba9e9850098710418e134f3d667b8606cc82
bercea17 <148089716+bercea17@users.noreply.github.com>
2023-11-06 18:58:24 +0200

    lab 5

---
+++ Employee.java
@@ -0,0 +1,23 @@
+public class Employee {
+    private String name;
+
+    private String email;
+
+    private Integer houreRate;
+
+    private Integer capacity;
+
+    private Integer freeDays;
+
+    private  Integer calculateDailyIncome() {
+        return houreRate * capacity;
+
+    }
+    public void  calculateMonthlyIncome() {
+        System.out
+
+    }
+
+
+}
+
